fbced5381a6a6f41199222f66916af556ac216b4
Jae-Beom, Park <kurdizle@gmail.com>
2021-05-23 20:07:16 +0900

    로그아웃, 유저 확인, 새로고침 메소드 추가

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,11 +34,44 @@ class AuthController extends Controller
         return $this->login($user);
     }
 
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(Auth::user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Auth::logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::refresh());
+    }
+
     protected function respondWithToken($token)
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
+            'user' => Auth::user(),
             'expires_in' => Auth::factory()->getTTL() * 60,
         ]);
     }
